8198904aece3652407a6152a6942ee97bc4b4539
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-03-01 14:06:34 -0500

    Adding color code for rating of Agents

--- src/public/residential.js
+++ src/public/residential.js
@@ -8,11 +8,20 @@ fetch('http://localhost:3004/agents')
             row.insertCell(0).innerHTML = j + 1
             row.insertCell(1).innerHTML = datum.first_name
             row.insertCell(2).innerHTML = datum.last_name
-            row.insertCell(3).innerHTML = datum.region
-            row.insertCell(4).innerHTML = datum.rating
-            row.insertCell(5).innerHTML = `$${datum.fee}`
+            row.insertCell(3).innerHTML = datum.rating
+            if (datum.rating == 100) {
+                myTable.rows[j].cells[3].style.color="#299429"
+            } else if ((90 <= datum.rating) && (datum.rating < 100)) {
+                myTable.rows[j].cells[3].style.color="#207cd7"
+            } else {
+                myTable.rows[j].cells[3].style.color="#8314c7"
+            }
+            row.insertCell(4).innerHTML = `$${datum.fee}`
+            // row.insertCell(5).innerHTML = datum.region
             j++
         });
+        // myTable.rows[1].cells[3].style.color="#FF0000"
     })
+
 
 
